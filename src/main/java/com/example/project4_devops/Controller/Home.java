2fa3717d691b99b1b8f740eb4429f091d1db0af3
ozdemirosman63 <ozdemir6483@gmail.com>
2025-05-16 00:38:22 +0300

    Fix: corrected @GetMapping path for /hello endpoint

--- src/main/java/com/example/project4_devops/Controller/Home.java
+++ src/main/java/com/example/project4_devops/Controller/Home.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Home {
+
     @GetMapping("/hello")
-    public String getIndex(){
+    public String getIndex() {
         return "Hello Kubernetes";
     }
 }

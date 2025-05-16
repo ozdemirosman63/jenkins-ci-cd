784916f512876f3d1a4a22f746cea47b988ad2cb
ozdemirosman63 <ozdemir6483@gmail.com>
2025-05-16 03:31:33 +0300

    e full test

--- src/main/java/com/example/project4_devops/Controller/Home.java
+++ src/main/java/com/example/project4_devops/Controller/Home.java
@@ -8,7 +8,7 @@ public class Home {
 
     @GetMapping("/")
     public String home() {
-        return "Hello webhook Kubernetes";
+        return "Hello webhoofdk Kubernetes";
     }
     @GetMapping("/index")
     public String getIndex() {

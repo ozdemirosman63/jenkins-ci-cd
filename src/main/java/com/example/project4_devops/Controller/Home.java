0555027f8f4a44a9a4c20a502735a91ade384ad0
Osman Özdemir <122050054+ozdemirosman63@users.noreply.github.com>
2025-05-16 05:51:52 +0300

    Update Home.java

--- src/main/java/com/example/project4_devops/Controller/Home.java
+++ src/main/java/com/example/project4_devops/Controller/Home.java
@@ -8,7 +8,7 @@ public class Home {
 
     @GetMapping("/")
     public String home() {
-        return "selamm";
+        return "selaFADSFASDFmm";
     }
     @GetMapping("/index")
     public String getIndex() {
